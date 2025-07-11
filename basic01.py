86675d293f78b025144510044e1a3c55d5cf20be
Fareed Abdul <abdulfareed355@gmail.com>
2025-07-11 23:17:09 +0530

    Daily update

---
+++ basic01.py
@@ -0,0 +1,33 @@
+#swapping in 3 methods 
+# 1 method is using temp varaible
+# x = 10;
+# y = 20;
+# temp = x; ''' ---> temp = 10'''
+# x = y; '''x === 20'''
+# y = temp; '''y === 10'''
+
+# print(x);
+# print(y);
+
+'''method 2 ---> without using the temp variable '''
+
+# x = 10;
+# y = 36;
+
+# [x,y] = [y,x]
+
+# print(x)
+# print(y)
+
+'''method 3 ---> with the operator'''
+
+# x = 20;
+# y = 10;
+
+
+# x = x+y 
+# y = x-y
+# x = x-y 
+
+# print(x)
+# print(y)
