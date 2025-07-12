477dbbe75a7651124d59d26fec64587878ce680a
Fareed Abdul <abdulfareed355@gmail.com>
2025-07-12 09:18:54 +0530

    Daily update

--- basic01.py
+++ basic01.py
@@ -31,3 +31,45 @@
 
 # print(x)
 # print(y)
+
+# name = "Fareed"
+# age  = 21
+# city  = "Hyderabad"
+# print(name, "is my name ok");
+# print( "i am" ,int(age) ,"years old")
+# print( "i live in", city)
+
+# favourite_color = "skyBlue"
+# print(favourite_color)
+
+# a = 5
+# b = 10
+
+# a,b =  b,a 
+
+# print(a)
+# print(b)
+
+# name = input("what's your name")
+# age = input("and you age")
+
+# print(f"Hello {name}, you are {age} years now") # f = formatted string that can understand the {} braces in the code
+
+
+# input1 = int(input("Enter the value :"))
+# input2 = int(input("Enter the value :"))
+
+# print(input1 + input2)
+
+# input1 = int(input("Enter the birth year :"))
+# current_year = 2025
+# age = input1 - current_year
+# print(f"You are {age} years old and born on {input1}")
+
+# input1 = input("Enter something: ")
+
+# if type(input1) == str:
+#     print("Your Input is a int")
+# else:
+#     print("Your input is not a int")
+        
