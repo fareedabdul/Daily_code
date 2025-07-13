57eb2a2bc21bea935b93df898fab873c244f3795
Fareed Abdul <abdulfareed355@gmail.com>
2025-07-13 21:16:22 +0530

    Daily update

--- Day02_operators.py
+++ Day02_operators.py
@@ -1,3 +1,5 @@
+
+# 1️⃣ Take a number from the user and check:
 # input1 = int(input("Enter the Number :"))
 
 # if input1 < 0:
@@ -7,8 +9,10 @@
 # else:
 #     print("its a zero")
 
-# age = int(input("Enter the age :"))
 
+# 2️⃣ Take age as input and print:
+
+# age = int(input("Enter the age :"))
 # if age < 13:
 #     print("you are a child Beta")
 # elif 13 <= age < 19:
@@ -20,11 +24,57 @@
 # else:
 #     print("Enter your age")
 
+# 3️⃣ Ask the user to enter username and password.
+# username = input("Enter the username :")
+# password = int(input("Enter the password :"))
 
-username = input("Enter the username :")
-password = int(input("Enter the password :"))
+# if username == "Fareed" and password == 1234:
+#     print("Login Successfull ")
+# else:
+#     print("access denied")
 
-if username == "Fareed" and password == 1234:
-    print("Login Successfull ")
-else:
-    print("access denied")
+
+
+# 4 Ask for marks (0–100) and print grade:
+
+
+# input_marks = int(input("Enter the Marks"))
+
+# if input_marks > 90:
+#     print("Grade A")
+# elif 75 <= input_marks <= 89:
+#     print("Grade B")
+# elif 60 <= input_marks <= 74:
+#     print("Grade C")
+# elif 35 <= input_marks <= 59:
+#     print("Grade D")
+# elif input_marks < 35:
+#     print("Grade F")
+# else:
+#     print("Invalid marks entered! Please enter marks between 0 and 100.")
+
+
+
+# 5️⃣ Build a mini calculator:
+# Ask for two numbers and an operator (+, -, *, /)
+
+# Perform and print the result
+
+
+# num1 = float(input("Enter the first number: "))
+# operator = input("Enter the operator (+, -, *, /): ")
+# num2 = float(input("Enter the second number: "))
+
+# if operator == '+':
+#     print("Result:", num1 + num2)
+# elif operator == '-':
+#     print("Result:", num1 - num2)
+# elif operator == '*':
+#     print("Result:", num1 * num2)
+# elif operator == '/':
+#     if num2 != 0:
+#         print("Result:", num1 / num2)
+#     else:
+#         print("Error: Cannot divide by zero.")
+# else:
+#     print("Invalid operator!")
