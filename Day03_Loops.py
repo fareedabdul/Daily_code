49593e778a26848a1c17e8e7f2d56f66d267d47a
Fareed Abdul <abdulfareed355@gmail.com>
2025-07-15 20:48:10 +0530

    Daily update

--- Day03_Loops.py
+++ Day03_Loops.py
@@ -2,9 +2,16 @@
 #     print(i)
 
 
-i = 1 # start counting from 1
-while i <= 10: # Jab tak i ki value 10 se chhoti ya barabar hai, tab tak loop chalega
-    #  Matlab: Jab tak i = 1, 2, 3, 4, 5 — tab tak chalna hai
-# Jaise hi i = 11 hota hai, loop ruk jaayega
-    print(i)
-    i = i + 1  #i ki value badhaayega har baar
+# i = 1 # start counting from 1
+# while i <= 10: # Jab tak i ki value 10 se chhoti ya barabar hai, tab tak loop chalega
+#     #  Matlab: Jab tak i = 1, 2, 3, 4, 5 — tab tak chalna hai
+# # Jaise hi i = 11 hota hai, loop ruk jaayega
+#     print(i)
+#     i = i + 1  #i ki value badhaayega har baar
+
+
+# i = 10
+# while i >= 1:   # i <= 10 ye logic -(minus mein jayegaa toh samjjh)
+#     print(i)
+#     i = i - 1
+
