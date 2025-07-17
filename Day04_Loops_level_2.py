2a9244b023c8423124b60bfc563353879f4bf012
Fareed Abdul <abdulfareed355@gmail.com>
2025-07-17 23:32:51 +0530

    Daily update

---
+++ Day04_Loops_level_2.py
@@ -0,0 +1,19 @@
+#  Problem 1: Count from 1 to 10
+# i = 1
+# while i <=10:
+#     print(i)
+#     i = i + 1
+
+# i = 2
+# while i <= 20:
+#     if i % 2 == 0:
+#         print(i)
+#     i += 1
+
+while True:
+    password  = input("Enter the password mamu:")
+    if password == "1":
+        print("Access Granted")
+        break
+    else:
+        print("Try again")
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,5 @@
-git add . && git commit -m "Daily update" && git push 
+i = 2
+while i <=20:
+    if i % 2 == 0:
+        print(i)
+        i += 1
