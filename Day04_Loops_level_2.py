348a6100aeff72612e579345e793993d04219ed9
Fareed Abdul <abdulfareed355@gmail.com>
2025-07-18 22:12:28 +0530

    Daily update

--- Day04_Loops_level_2.py
+++ Day04_Loops_level_2.py
@@ -10,10 +10,27 @@
 #         print(i)
 #     i += 1
 
-while True:
-    password  = input("Enter the password mamu:")
-    if password == "1":
-        print("Access Granted")
-        break
-    else:
-        print("Try again")
+# while True:
+#     password  = input("Enter the password mamu:")
+#     if password == "python123":
+#         print("Access Granted")
+#         break
+#     else:
+#         print("Try again")
+
+# num = int(input("Enter the table number: "))
+# i = 1
+# while i <= 10:
+#     print(f"{num} x {i} ={(num * i)}")
+#     i +=1
+
+# Take an input n from user and find the sum of all numbers from 1 to n using a while loop.
+
+# num = int(input("Enter the number to sumup:"))
+# i = 1
+# total = 0
+# while i <= num:
+#     total += i
+#     i += 1
+#     print("The total sum is:", total)
+
