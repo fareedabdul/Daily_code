1a76a7ac4e65f9c2b2f2887525d78e6c28d8fc1f
Fareed Abdul <abdulfareed355@gmail.com>
2025-07-22 19:39:48 +0530

    Daily update

---
+++ Day4.5_slicing.py
@@ -0,0 +1,34 @@
+# s = "There is a Good Boy"
+# print(s[:5:])
+
+# s = "There is a Good Boy"
+
+# print(s[6:15:])  //is a Good
+
+# s = "There is a Good Boy"
+
+# print(s[:-9:-1])  yoB dooG
+
+
+# title = "Fareed is a good boy"
+# print(title[-18])
+# print(s[-4])
+
+# Modifying the string 
+
+# text1 = "Fareed is"
+# text1 = "Mr." + text1
+# text1 = text1 + " Learning Python Mama"
+# # text1 = text1.replace("python","javascript")
+# # print(text1[:28] )
+# text1 = text1[:28] + " Programming" + text1[28:] 
+# # **************
+# # text1[:28] ==== Mr.Fareed is Learning Python
+# # adding the programming after 28 means python
+# # then showing the remaining text from 28 
+
+# text1 = text1[:21] + " Adv" + text1[21:]
+
+# text1 = text1[:21] + text1[44:]
+# print(text1)
+
