31731e466d2e0602edff068dca3faea1b222a199
Fareed Abdul <abdulfareed355@gmail.com>
2025-07-22 09:38:12 +0530

    Daily update

---
+++ Day05_FunctionL1.py
@@ -0,0 +1,37 @@
+# def function(name):
+#     print(f"Hello ,{name}")
+
+# function("Fareed")
+
+
+# def add(a,b,c):
+#     return a+b+c
+# result = add(5,6,1) 
+# print(f"this are the 3 adding number of {result}")
+
+# Function that multiplies two numbers and returns the result.
+
+# def multiply(num1,num2):
+#     return num1 * num2
+# input1 = int(input("Enter the first number"))
+# input2 = int(input("ENter the second number"))
+# result = multiply(input1,input2)
+# print(f"the multiplication of two numbers are {result}")
+
+# Function that takes a string and prints it in uppercase.
+# def function(input1):
+#     return input1.upper()
+
+# result = function("fareed")
+# print(result)
+
+# Function that takes your birth year and returns your age.
+
+# def calculate_age(birth_year):
+#     current_year = 2025
+#     return current_year - birth_year
+
+# input1 = int(input("Enter your birth year: "))
+# result = calculate_age(input1)
+# print("Your age is:", result)
+
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,1 @@
-i = 2
-while i <=20:
-    if i % 2 == 0:
-        print(i)
-        i += 1
+function(f"Fareed")
