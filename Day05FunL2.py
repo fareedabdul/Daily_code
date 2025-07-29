57653b26daadb0597d0217fe38fd6492a8c62975
Fareed Abdul <abdulfareed355@gmail.com>
2025-07-29 23:04:14 +0530

    Daily update

--- Day05FunL2.py
+++ Day05FunL2.py
@@ -1,10 +1,45 @@
 ###########################################
 # Leap Year 
-def leap_year(year):
-    if (year % 4 == 0 and year % 100 != 0) or (year % 400 ==0 ):
-        return True 
-    else:
-        return False
-input = int(input('Enter the Year: '))
-result = leap_year(input)
-print(result)
+# def leap_year(year):
+#     if (year % 4 == 0 and year % 100 != 0) or (year % 400 ==0 ):
+#         return True 
+#     else:
+#         return False
+# input = int(input('Enter the Year: '))
+# result = leap_year(input)
+# print(result)
+
+##################################################################3
+
+# prime number:
+
+# def prime_number(n):
+#     count = 0
+#     for i in range(2,n):
+#         if n % i == 0:
+#             count = count+1
+#             break
+
+#     if count == 0  and n > 1:
+#         print("its a prime")
+#     else:
+#         print("Not a prime")
+
+# n = int(input("Enter your marzi number: "))
+# prime_number(n)
+
+
+# def prime(inp):
+
+#     if inp <= 1:
+#         return f"Its not a prime number"
+#     for i in range(2,inp):
+#         if inp % i == 0:
+#             return f"its not a prime"
+#         else:
+#             return f"its a prime"
+        
+# result = int(input("Enter the input :")) 
+# output = prime(result)
+# print(output)
+
