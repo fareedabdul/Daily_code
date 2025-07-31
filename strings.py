bfae2aa1674d164684aa5257c643624d62c37564
Fareed Abdul <abdulfareed355@gmail.com>
2025-07-31 07:34:55 +0530

    Daily update

--- strings.py
+++ strings.py
@@ -1,4 +1,4 @@
-s = "Fareed is"
+# s = "Fareed is"
 # ls = s[-1:-3:-1]
 # ls = s[-1:-9:-2]
 # ls = s[5:1:-1] #---Imp---
@@ -14,5 +14,15 @@ s = "Fareed is"
 # ls = s[-3:-6:-2]
 # ls = s[-4:2:-1]
 # ls = s[-2:-2:-1] // it will return the Nothing
-ls = s[-3:-9:-3]
-print(ls)
+# ls = s[-3:-9:-3]
+# print(ls)
+
+
+#-----------------indirect way to access the strings-----
+# s = "So hello"
+# s = "GM " + s
+# s = s + " Hie EveryOne"
+# s = s[0:2] + " guys " + s[3:]
+# s = s[0:2] + " Good Mrng Guys"
+# s = s[3:]
+# print(s)
