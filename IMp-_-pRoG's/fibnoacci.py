23d167f9cd05c6c6d522b616b6680d58b6126a04
Fareed Abdul <abdulfareed355@gmail.com>
2025-08-02 23:05:00 +0530

    Daily update

--- IMp-_-pRoG's/fibnoacci.py
+++ IMp-_-pRoG's/fibnoacci.py
@@ -1,8 +1,25 @@
-def fibnacci(num):
-    if num ==0 or num == 1:
+# def fibnacci(num):
+#     if num ==0 or num == 1:
+#         return num
+#     else:
+#         return fibnacci(num - 1) + fibnacci(num - 2)
+# input = int(input("Enter the number for Fib haan : "))
+# result = fibnacci(input)
+# print(result)
+
+# Method 2:
+def fibonacci(num):
+    fareed = 0
+    roshan = 1
+    if num == 1 or num == 0:
         return num
-    else:
-        return fibnacci(num - 1) + fibnacci(num - 2)
-input = int(input("Enter the number for Fib haan : "))
-result = fibnacci(input)
+    for i in range(2,num+1):
+        temp = fareed +roshan
+        fareed = roshan 
+        roshan = temp
+    return roshan
+
+input = int(input("Enter the Number of fib: "))
+result = fibonacci(input)
 print(result)
+
