fe68e7fd4e67e461e424c55f8f3026e78189080f
Fareed Abdul <abdulfareed355@gmail.com>
2025-08-14 21:57:02 +0530

    Daily update

--- Strings_practise.py
+++ Strings_practise.py
@@ -1,3 +1,23 @@
 
 #! 1. what is a string in python 
 #? string in python is a collection of characters or words that play a very imp role in python for understanding python syntax and all other stuff where 2. python is immutable datatype 3. that cant be changed once kit allocated ya we can change it by the indirect access 4. and python has 29 methods for doing everything that can be able to do,, 5. it has many way to create by the "single double quotes" & "" double quotes"" & 'single quotes'
+
+#!2. how are string defined ?
+#? "single double quotes" & "" double quotes"" & 'single quotes'
+
+#! 3. what is the difference between single, double , and triple quotes in python string 
+#? ookay first two are same and only triple quotes that will store the strings in multi-lines okay thats all
+
+#! 4. strings are immutable 
+# s = "String"
+# s[0] = "f"
+# print(s) #?'str' object does not support item assignment
+#? strings are immuatable we cant change the data once it fixed 
+
+
+#! 5. How do you access the strings by indexing 
+#? we can access the string by index like by its index position its simple as that 
+
+# s= "strings"
+# print(s[5])
+
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,1 @@
-
-# print(s)
+ its simple as that 
