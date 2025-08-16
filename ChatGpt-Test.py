29e4e07d511b6d83827959a88b53ae04d37eb075
Fareed Abdul <abdulfareed355@gmail.com>
2025-08-16 21:36:05 +0530

    Daily update

---
+++ ChatGpt-Test.py
@@ -0,0 +1,19 @@
+# Theory Questions (Python Basics)
+
+# Try answering these in your own words:
+
+#? What is Python, and why is it called an interpreted language?
+#* Python is general purpose programming languages and its a high level language, and easy syntax and easily to read, interpreted means line by line execution, python executes line by line instead of at once 
+#? What are variables in Python? Give an example.
+#* varaibles are like stomach and values like biryani
+# What is the difference between a list, tuple, and set in Python?
+#* 
+# Explain if-elif-else statements with an example.
+
+# What is a function? Why do we use functions?
+
+# What are Python data types? Name at least 5.
+
+# What is the difference between == and = in Python?
+
+# Explain what a loop is and the difference between for and while loops.
