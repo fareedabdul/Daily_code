d820b62ff194a39e9b67d69a4805faa82d024cb9
Fareed Abdul <abdulfareed355@gmail.com>
2025-08-20 21:20:43 +0530

    Daily update

---
+++ TypeCast(float).py
@@ -0,0 +1,11 @@
+# 
+#! float is executed in only primitive not in non primitve instead of string
+
+#Float
+f = 3.8
+print(int(f))
+print(complex(f))
+print(bool(f))
+print(str(f))
+##########################################
+# for list and tuple and set and dict this cant be done
