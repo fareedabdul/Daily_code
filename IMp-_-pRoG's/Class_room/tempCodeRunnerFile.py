be449f08f90c13194282044b43c9678f1e3928e5
Fareed Abdul <abdulfareed355@gmail.com>
2025-08-29 22:44:56 +0530

    Daily update

--- IMp-_-pRoG's/Class_room/tempCodeRunnerFile.py
+++ IMp-_-pRoG's/Class_room/tempCodeRunnerFile.py
@@ -1,2 +1,22 @@
+# def sum_of_even(lst):
+#     total = 0
 
-if num % 2 == 0:
+#     for i in lst:
+#         if i % 2 ==0:
+#             total += i
+#     return total 
+
+# input = list(map(int,input("Enter the Number:").split()))
+# result = sum_of_even(input)
+# print(result)
+
+
+def is_palindrome(text):
+    
+    if text == text[::-1]:
+        return True
+    else:
+        return False
+input = input("Enter the number: ")
+out = is_palindrome(input)
+print(out)
