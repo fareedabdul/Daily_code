cf7c3a99df594d0618104a2a45886f555b161cd5
Fareed Abdul <abdulfareed355@gmail.com>
2025-09-01 18:57:14 +0530

    Daily update

---
+++ IMp-_-pRoG's/Class_room/Match_case.py
@@ -0,0 +1,17 @@
+###############################################################################################################
+
+# print("GREETINGS SIR/MADAM \n SELECT 1 FOR URDU \nSELECT 2 FOR hindi \nSELECT 1 FOR english \n")
+# select = int(input("Enter the number for language:"))
+
+# match select:
+#     case 1:
+#         print(f"you have selected {select} and the language is URDU")
+#     case 2:
+#         print(f"you have selected {select} and the language is hindi")
+#     case 3:
+#         print(f"you have selected {select} and the language is english")
+#     case _:
+#         print("please enter a valid number")
+
+##################################################################################################################
+
