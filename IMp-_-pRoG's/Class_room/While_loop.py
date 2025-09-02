a645344703f286a627bd1ed827a944dc433c94a9
Fareed Abdul <abdulfareed355@gmail.com>
2025-09-02 21:37:22 +0530

    Daily update

--- IMp-_-pRoG's/Class_room/While_loop.py
+++ IMp-_-pRoG's/Class_room/While_loop.py
@@ -24,13 +24,13 @@
 
 # Q3.WAP TO -1 TO -10
 
-start = -1
-end = -10
+# start = -1
+# end = -10
 
-while start >= end:
-    print(start)
+# while start >= end:
+#     print(start)
 
-    start -= 1
+#     start -= 1
 
 # Q4. WAP TO print -20 to -10
 
@@ -41,3 +41,33 @@ while start >= end:
 #     print(start)
 
 #     start += 1
+
+
+# Q1.WAP  to print 1 - 5
+
+# start = 1 
+
+# while start <= 5:
+#     print(start)
+
+#     start += 1
+
+# Q2.WAP to print 3 to 1 
+# start = 3
+# end = 1 
+
+# while start >= end:
+#     print(start)
+
+#     start -= 1
+
+# Q3. WAP to print even number from 2 to 15 
+
+# start = 2 
+# end = 15 
+
+# while start <= end:
+#     print(start)
+
+#     start +=2
+
