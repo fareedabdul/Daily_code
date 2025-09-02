6409bc220649983d3efe117ca4018be23048a30d
Fareed Abdul <abdulfareed355@gmail.com>
2025-09-02 21:12:35 +0530

    Daily update

---
+++ IMp-_-pRoG's/Class_room/While_loop.py
@@ -0,0 +1,17 @@
+# WAP TO PRINT ODD NUMBER FROM 2 TO 10 
+
+# INTILAIZATION 
+# CONDITION 
+# INC/DEC
+
+# start = 2
+# end =10
+
+# while start <= 10:
+#     if start % 2 != 0:
+#         print(start)
+
+#     start += 1
+
+# Q2. WAP to print 100 to 50 even number
+
