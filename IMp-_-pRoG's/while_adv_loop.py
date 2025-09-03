2dadda7e0da84ee17e6b2eaa8f90fada5e9ae64f
Fareed Abdul <abdulfareed355@gmail.com>
2025-09-03 21:00:54 +0530

    Daily update

---
+++ IMp-_-pRoG's/while_adv_loop.py
@@ -0,0 +1,17 @@
+# write to create a list of even numbers and tuple of odd numbers 1 - 20
+
+# start = 1 
+# end = 20
+# even = []
+# odd = ()
+
+# while start <= end:
+#     if start % 2 == 0:
+#         even.append(start)
+#     else:
+#         odd += (start,)
+#     start += 1
+# print(even,odd,sep="\n")
+
+
+# WAP to print A - Z
