c771d15740b7578c4d6871d32a4c6930f432d6ff
Fareed Abdul <abdulfareed355@gmail.com>
2025-09-05 21:49:17 +0530

    Daily update

---
+++ IMp-_-pRoG's/Class_room/For_loop/For_loop.py
@@ -0,0 +1,40 @@
+# for syntax
+# for ref_variable_name in iterable
+
+# 1) WAP TO ITERATE OVER A STRING AND GET ONE CHAR AT A TIME 
+
+# s = "Fareed"
+
+# for i in s:
+#     print(i)
+# -----------------------------------------------------------------------------
+# 2) WAP TO ITERATE OVER A LIST AND GET ONE CHAR AT A TIME 
+
+# l = [77,99,2,9.99,"hi"]
+# for i in l:
+#     print(i)
+# .............................................................................
+# 3) WAP TO ITERATE OVER A TUPLE AND GET ONE CHAR AT A TIME 
+
+# t = (77,99,2,9.99,"hi")
+
+# for o in t:
+#     print(o)
+
+# ---------------------------------------------------------------
+# 4) WAP TO ITERATE OVER A set AND GET ONE CHAR AT A TIME 
+
+
+# s = {77,99,2,9.99,"hi"}
+
+# for i in s :
+#     print(i)
+
+
+# -------------------------------------------------------------------
+# 5) WAP TO ITERATE OVER A DICT AND GET ONE CHAR AT A TIME 
+
+s = {7: "Fareed", 31 : "bhaiya", 25: "mom"}
+
+for i in s:
+    print(i)
