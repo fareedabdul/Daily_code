2b724c8f81af0a0a30c927d6bc4db5713acb2696
Fareed Abdul <abdulfareed355@gmail.com>
2025-09-07 11:31:18 +0530

    Daily update

---
+++ IMp-_-pRoG's/Class_room/while_Loop/Test.py
@@ -0,0 +1,45 @@
+# 1.PRINT THE SQUARES OF NUMBER FROM 1 TO 10 
+
+# start = 1
+# end = 10 
+# while start <= end:
+#     print(start ** 2)
+
+#     start += 1
+
+# 2. print all number divisible from 1 to 50 that are divisible by 7:
+
+
+# start = 1
+# end = 50
+
+# while start <= end:
+#     if start % 7 ==0:
+#         print(start)
+#     start += 1
+
+
+# 3. print sum from 1 to 100 
+
+
+# start = 1 
+# end = 100 
+# sum = 0
+
+# while start <= 100:
+#     sum += start 
+#     start +=1 
+
+# print(sum)
+
+
+# 4. PRINT NUMBERS FROM 1 TO 100 BUT ONLY THOSE DIVISIBLE BY BOTH 3 AND 5 
+
+# start = 1
+# end = 100 
+
+# while start <= end :
+#     if start % 3 == 0 and start % 5 == 0:
+#         print(start)
+#     start += 1
+
