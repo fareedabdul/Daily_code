e46fa3ab752196e38478ddd359a80279ad9ed2fc
Fareed Abdul <abdulfareed355@gmail.com>
2025-09-08 23:21:40 +0530

    Daily update

--- IMp-_-pRoG's/Class_room/For_loop/For_loop02.py
+++ IMp-_-pRoG's/Class_room/For_loop/For_loop02.py
@@ -1,6 +1,13 @@
 # Q) WAP TO REVERSE LIST
 
-li = [55,44,33,45]
+# li = [55,44,33,45]
 
-for i in range(len(li)-1,-1,-1):
-    print(li[i],end=" ") 
+# for i in range(len(li)-1,-1,-1):
+#     print(li[i],end=" ") 
+
+# Q) WAP TO REVERSE TUPLE AND PRINT ALTERNATE VALUE
+
+tup = (88,77,55,"Siddhu")
+
+for i in range(len(tup)-1,-1,-2):
+    print(tup[i])
