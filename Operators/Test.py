53f6bc4cc98e8b127e7e24e671855dc69a7b74ae
Fareed Abdul <abdulfareed355@gmail.com>
2025-09-10 13:02:43 +0530

    Daily update

---
+++ Operators/Test.py
@@ -0,0 +1,115 @@
+# ** Imp for interview 
+
+# how many types of operators are present in python and tell me all those ? 
+
+# answer there are 8 types of operators are there 
+# shortcut/ code  A T M A C B I L 
+
+#1 Assignment operator 
+# assignment are used assign the value to the operators or values there are basically two types of assignmnet operators are present 
+# 1st is Direct assignment 
+# 2nd is compound assignmnet
+
+# direct assignment is like  
+# example 
+# a = 10 
+
+# 2nd -- compound is combination of arithmetic and bitwise operator is called compund assignmnet basically used for memory saving methods and one of part is append
+
+
+# a = 10 
+# a += 1
+# print(a) # 11
+
+# _---------------------------------------------------------------------------------------------------------------------------
+
+
+# 2nd  Ternary operator
+
+# ternary operator is used to shortand the statment for only the conditional statment  
+
+# syntax : True value block if Condition else false value block
+
+
+# marks = 36
+# " Pass" if marks > 35 else "fail"
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+# 3. comaparison operator 
+# comparison opr is used to compare the vales not the data types 
+
+# there are 6 types are basically :
+# ==  equals
+# >  greater than 
+# < less than 
+# >=  greater than 
+# <=  less than 
+
+
+# --------------------------------------------------------------------------------------------------------------------------------------/
+
+# 4. Membership operator : 
+# membership operator is used to see is a sequence is present in or not 
+ 
+# in 
+# not in 
+
+
+# print("s" in "siddhu")  # True
+# print("s" not in "siddhu")  # False
+
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+# 5 . arithmetic operator 
+
+# + addtion Note: cant be dont int plus dictanary 
+# - Subtraction Note : cant be dont in Collections 
+# * Multiplication we can do in string too Ex "siddhu" * 5 = siddhu siddhu siddhu siddhu siddhu
+# / Divison(Quotient)
+# % Modulo (Remainder)
+#: // Floor opr returns the closest value in the number
+# ** Exponent square or cube 5 ** 2 # 25
+
+# -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# 6. Bitwise opr 
+
+# first it will only deals with integers, first it will convert integers into bits and do the remaining operations 
+# there are 6 types are there :
+# # 1) Bitwise and   : Both conditions should be Meet
+# 2) Bitwise not : Reverse of answer
+# 3) Bitwise Xor : if both Will same resultant will be False 
+# 4) Bitwise or : even one True then its a True
+# 5) Bitwise Rightshift >> : Note : Data Loss
+# 6) Bitwise Left Shift << : Note : No Data Loss
+
+# ---------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+# 7. Identity operator 
+
+# identity opr is likely to compare the addresses
+# there are Two types of opr 
+# 1. is  : if both the address is same then its True
+# 2. is not : if bot the address different then the resultent will be False
+
+# --------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# 8. Logical operator 
+
+# logical deals with condtional statments and the values 
+
+#  3 Types of Logical operator
+# 1. and : both Condition should be Meet
+# 2. or  : even One is True is enough 
+# 3. not : Negation it is so reverse of the answer 
+
+
+
+
+# *********************************************************End of Operators ***********************************************
+
