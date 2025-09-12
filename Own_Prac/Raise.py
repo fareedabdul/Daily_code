a7878f11c706e9d0f293067f81c4bfad2365f361
QUANTUM'S FAREED <143338196+fareedabdul@users.noreply.github.com>
2025-09-12 21:56:22 +0530

    Update Raise.py

--- Own_Prac/Raise.py
+++ Own_Prac/Raise.py
@@ -1,5 +1,11 @@
-x = -5
-if x < 0:
-  raise valueError('x should be postive')
+ #x = -5
+# if x < 0: 
+#  raise valueError('x should be postive')
 
 #raise is a manuall way to throw the exception 
+
+
+try:
+  num = int("abc")
+except valueError as e:
+  print("caught an error",e)
