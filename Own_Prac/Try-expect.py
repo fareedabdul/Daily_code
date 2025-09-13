3650982ab1a5dad6b8c4914bef03b4bda8532047
QUANTUM'S FAREED <143338196+fareedabdul@users.noreply.github.com>
2025-09-13 21:00:30 +0530

    Update Try-expect.py

--- Own_Prac/Try-expect.py
+++ Own_Prac/Try-expect.py
@@ -3,4 +3,12 @@ try:
     print(n/0)
 except ZeroDivisionError:
     print("aab uhtauch")
-      
+
+
+l = [77,88,66,99]
+try:
+    print(l.index(1000))
+except ValueError:
+    print("this value which you passed is not found")
+except IndexError:
+    print("This index is not found")
