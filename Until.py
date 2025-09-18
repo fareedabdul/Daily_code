d2095664e99d7f75b3050cdfb9841ae4fbad7774
QUANTUM'S FAREED <143338196+fareedabdul@users.noreply.github.com>
2025-09-18 22:44:52 +0530

    Update Until.py

--- Until.py
+++ Until.py
@@ -5,3 +5,15 @@ new_fun(10,20)
 def new_fun(a,b)
     ...
 print(new_fun(a=10,20))
+
+
+# def demo(name,age):
+#     ...
+# print(demo("Fareed",21))
+
+#? Hamesha function ke andar kuch operation (return ya print ya calculation) hona chahiye, tabhi woh kuch output dikhayega.
+
+
+def demo(name,age):
+    print(name,age)
+print(demo("fared",21))
