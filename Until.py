5a784c4558146755734c26e3a3da382c2caad58d
QUANTUM'S FAREED <abdulfareed355@gmail.com>
2025-10-08 21:52:22 +0530

    Force push local files

--- Until.py
+++ Until.py
@@ -28,4 +28,4 @@
 # #####################################################################################################################
 # # class and object 
 # Class :- Class is a blueprint or template that tells objects what actually object should perform or behave
-print("heLLOW")
+# print("heLLOW")
