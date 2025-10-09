01215e979a5bb283f216e8ab785a6cc493c93392
QUANTUM'S FAREED <abdulfareed355@gmail.com>
2025-10-09 21:36:14 +0530

    Force push local files

---
+++ OOPS/Programs/basic.py
@@ -0,0 +1,16 @@
+# class Qsp:
+#     institute_name = "qspiders"
+#     manager = "Saiteja"
+#     location = "chaitanyapuri"
+#     pin = 500028
+    
+#     def __init__(self,name,age,subject,marital):
+#         self.trainer_name = name
+#         self.trainer_age = age
+#         self.trainer_subject = subject
+#         self.status_marriage = marital
+#     def __str__(self):
+#         return f'{self.trainer_name}, {self.trainer_age}, {self.trainer_name}, {self.status_marriage}'    
+# t1 = Qsp("Fareed",21,"python","single")
+# print(t1)
+# print(t1.trainer_name,t1.trainer_age,t1.trainer_subject,t1.status_marriage)
