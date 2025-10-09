f6459173ecaf348189991882bf640553cf351f84
QUANTUM'S FAREED <abdulfareed355@gmail.com>
2025-10-09 10:06:04 +0530

    Force push local files

---
+++ OOPS/program.py
@@ -0,0 +1,15 @@
+# class Sample:
+#     a = 10
+#     b = 20
+    
+# obj1 = Sample()
+# obj2 = Sample()
+
+# print(id(Sample))
+# print(id(obj1))
+# print(id(obj2))
+
+# # print(Sample.__dict__)
+# # print(dir(obj1))
+
+# print(obj1.__dict__)
