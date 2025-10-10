b082d585db6efe36eb3b2782603bd876661e7c31
QUANTUM'S FAREED <abdulfareed355@gmail.com>
2025-10-10 22:01:41 +0530

    Force push local files

--- OOPS/Programs/Methods.py
+++ OOPS/Programs/Methods.py
@@ -1,14 +1,41 @@
-class Student:
-    college = "Holy mary"
+# instance method
+
+# class Student:
+#     college = "Holy mary"
     
-    def __init__(self,name,age):
-        self.name = name
-        self.age = age
+#     def __init__(self,name,age):
+#         self.name = name
+#         self.age = age
         
-    def show_details(self):
-        print(f"NAME : {self.name}")
-        print(f"age : {self.age}")
-        print(f"college : {Student.college}")
+#     def show_details(self):
+#         print(f"NAME : {self.name}")
+#         print(f"age : {self.age}")
+#         print(f"college : {Student.college}")
         
-obj1 = Student("Fareed",21)
-obj1.show_details()
+# obj1 = Student("Fareed",21)
+# obj1.show_details()
+
+# --------------------------------------------------
+# Class Method
+
+
+# class Student:
+#     college = "Holy Mary"
+    
+#     @classmethod
+#     def chnge_clg(cls,new_name):
+#         cls.college = new_name
+        
+# Student.chnge_clg("HMC")
+# print(Student.college)
+
+
+# ------------------------------------------------
+# static method
+
+
+# class Student:
+#     @staticmethod
+#     def print():
+#         print("Welcome to all")
+# Student.print()
