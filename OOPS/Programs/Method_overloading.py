efdb7a1d54a8ce6a70dde191d2fe8956af6e08a5
QUANTUM'S FAREED <abdulfareed355@gmail.com>
2025-10-13 23:06:57 +0530

    Force push local files

--- OOPS/Programs/Method_overloading.py
+++ OOPS/Programs/Method_overloading.py
@@ -1,9 +1,18 @@
 # class Over:
-#     def mama(a,b):
+#     def mama(self,a,b):
 #         print(a+b)
-#     def mama(a,b,c):
+#     def mama(self.a,b,c):
 #         print(a+b+c)
         
 # obj1 = Over()
 # Over.mama(1,2,3)
         
+        
+# class Default:
+#     def gs(self,*args):
+#         total = 0
+#         for i in args:
+#             total += i
+#         print(total)
+# d1 = Default()
+# d1.gs(2,6,5)
