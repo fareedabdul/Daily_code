23aafd153e59b1076bf2f1e6f808f9041bdede8b
QUANTUM'S FAREED <abdulfareed355@gmail.com>
2025-10-15 22:48:45 +0530

    Force push local files

---
+++ Programming/1_to_40.py
@@ -0,0 +1,134 @@
+# 1. Write a statement (WAS) to print "Hello World" by using shell
+#sol :-
+#  "Hello World"
+
+# 2. WAS to print "Hello World " by using python print Function
+# print("Hello World")
+
+#3. WAS to initialize variable and value as 50.
+# a = 50
+
+# 4 WAS to initialize multivaraible(values are 150, 120, 250)
+# (Take your own variable names)
+# a,b,c = 10,20,30
+
+# 5 Print the type of a given value.
+# a = 10
+# print(type(a))
+
+# 6. Print the memory address of a given value.
+# a = "String"
+# print(id(a))
+
+# 7. WAS to print your detials, first store your details, extract the values and display it 
+# name = "Fareed" 
+# age = 21
+# location = "Hyd"
+# print(f"My name is {name} and age is {age} and my location is {location}")
+
+
+#8. Swap two values using a temporary variable.
+# a = 10
+# b = 20
+# temp = a 
+# a = b
+# b = temp
+# print(a)
+# print(b)
+
+# 9. Swap two values without using a temporary variable.
+# a = 10
+# b = 20
+# a,b = b,a
+# print(a)
+# print(b)
+
+
+# 10. Initialize a variable, print it, reassign a new value, and print again.
+# a = 20
+# print(a)
+# a = 30
+# print(a)
+
+# Convert a single value to multi-value (e.g., 10 → ['1', '0']).
+# a = 10
+# a= list(str(a))
+# print(a)
+
+# Concatenate two multi-value data types (str, list, tuple, set, dict).
+s1 = "Hello"
+s2 = "world"
+print(s1 +s2)
+
+l1 = [1,2]
+l2 = [3,4]
+print(l1 + l2)
+
+
+st1 = {1,3}
+st2 = {5,6}
+print(st1 | st2)
+
+
+dt = {"name" : "Fareed" ,"age" :20}
+dt2 = {"loc" : "hyd"}
+print(dt | dt2)
+
+
+
+
+# Get the length of a collection.
+
+# Modify the middle value of a given collection.
+
+# Concatenate a new string at the start of a given string.
+
+# Concatenate a new string at the end of a given string.
+
+# Concatenate a new string in the middle of a given string.
+
+# Modify a character in the middle of a given string.
+
+# Modify the sequence of characters in a given string.
+
+# Replace an old character with a new character in a given string.
+
+# Delete a specific character from a string.
+
+# Delete a sequence of characters from a string.
+
+# Concatenate a new string at a specific position in a string.
+
+# Concatenate a new value at the start of a list.
+
+# Concatenate a new value at the end of a list.
+
+# Concatenate a new value in the middle of a list.
+
+# Concatenate a new value at a specific position in a list.
+
+# Modify a value at a specific position in a list.
+
+# Modify a value at the start of a list.
+
+# Modify a value at the end of a list.
+
+# Modify a value in the middle of a list.
+
+# Modify the first 4 values of a list.
+
+# Modify the sequence of values in a list.
+
+# Delete a value at a specific position in a list.
+
+# Delete a value from the end of a list.
+
+# Delete a value from the start of a list.
+
+# Delete a value from the middle of a list.
+
+# Concatenate a value at the start of a tuple.
+
+# Concatenate a value in the middle of a tuple.
+
+# Concatenate a value at the end of a tuple.
