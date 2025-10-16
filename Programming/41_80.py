4b6513f0d5b4f9c540e8721fd0cd323a01249822
QUANTUM'S FAREED <abdulfareed355@gmail.com>
2025-10-16 22:12:49 +0530

    Force push local files

---
+++ Programming/41_80.py
@@ -0,0 +1,17 @@
+# 70 WAE to check the given number is odd and it should be greater than 97 
+
+# num = int(input("Enter the Num: "))
+# if num % 2 != 0 and num > 97:
+#     print("Condition is True")
+# else:
+#     print("Nope This is wrong")
+    
+    
+# 71. WAE  to check the given number is less than 122 and greater than 48 and the number should be divisible by 4 
+
+# num = int(input("Enter the num: "))
+# if num < 122 and num > 48 and num % 4 == 0:
+#     print("Condn is True")
+# else:
+#     print("Nope Woh galat hai")
+
