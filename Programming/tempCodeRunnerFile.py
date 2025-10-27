d36cd984b109ff821a75928bef8e30e29eecc392
QUANTUM'S FAREED <abdulfareed355@gmail.com>
2025-10-27 22:35:06 +0530

    Force push local files

--- Programming/tempCodeRunnerFile.py
+++ Programming/tempCodeRunnerFile.py
@@ -1,5 +1,1 @@
-num = int(input("Enter the Num: "))
-if num >= 100 and num <= 200:
-    print("Condition is True")
-else:
-    print("Condtion is False")
+e
--- Programming/while_Entry.py
+++ Programming/while_Entry.py
@@ -1,2 +1,20 @@
 # Strong Number ek aisa number hota hai jiske digits ke factorials ka sum usi number ke barabar hota hai.
 
+n = int(input("Enter the Number: "))
+
+res = 0
+demo = n
+
+while n != 0:
+    ld = n % 10
+    fact = 1
+    while ld != 0:
+        fact *= ld
+        ld -= 1
+    res += fact
+    n //= 10
+
+if demo == res:
+    print("Strong hai")
+else:
+    print("Nahi hai Strong")
