f363b5b96ed607503a89a2d08ce239a0569bfd09
QUANTUM'S FAREED <abdulfareed355@gmail.com>
2025-10-28 21:49:49 +0530

    Force push local files

---
+++ Daily_code-master/File_handling/Day01.py
@@ -0,0 +1,17 @@
+import os
+
+# print(os.getcwd()) #for getting current directory
+# print(os.getcwd())
+# print(os.chdir("C:\Python_dev")) # it chnages the directory 
+# print(os.getcwd())
+# print(os.mkdir("hello world"))
+# os.rmdir("hello world") # removes the directory 
+
+# print(os.listdir())
+print(os.getcwd())
+os.chdir(r"C:\Python_dev")
+# print(os.mkdir("Hello world"))
+# print(os.mkdir("Hello world"))
+# print(os.rmdir("Hello world"))
+print(os.listdir())
+
