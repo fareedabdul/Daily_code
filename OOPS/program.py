ccd01c9ef752e4ba1457ed565b817ade5c4fddda
QUANTUM'S FAREED <abdulfareed355@gmail.com>
2025-11-03 22:34:59 +0530

    Force push local files

--- OOPS/program.py
+++ OOPS/program.py
@@ -15,12 +15,45 @@
 # print(obj1.__dict__)
 
 
-class data:
-    a = 10
-    b = 20
-obj1 = data()
-obj2 = data()
-print(obj2.a)
-print(obj1.a)
+# class data:
+#     a = 10
+#     b = 20
+# obj1 = data()
+# obj2 = data()
+# print(obj2.a)
+# print(obj1.a)
 
-class 
+# print(data.__dict__)
+# print(dir(obj1))
+
+# print(data.a)
+# print(data.b)  # its an class name okay
+
+# print(obj1.a)
+# print(obj1.b) # its an object name and its an obj1
+
+# print(obj2.a)
+# print(obj2.b) 
+
+class SampleData:
+    a = 10
+    b = 20 
+obj1 = SampleData()
+obj2 = SampleData()
+
+# print(SampleData.a)
+# print(obj1.a)
+# print(obj2.a)
+SampleData.a = 500
+print(SampleData.a)
+print(obj1.a)
+print(obj2.a)
+obj1.a = 777
+print(SampleData.a)
+print(obj1.a)
+print(obj2.a)
+SampleData.a = 800
+
+
+
+    
--- OOPS/tempCodeRunnerFile.py
+++ OOPS/tempCodeRunnerFile.py
@@ -1,9 +1,2 @@
-# class Sample:
-#     a = 10
-#     b = 20
-    
-# obj1 = Sample()
-# obj2 = Sample()
 
-# print(id(Sample))
-# print(id(obj1))
+print(obj1.a)
