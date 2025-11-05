e94f77ad65ab9958ece9119ce286c9ab2f22be7d
QUANTUM'S FAREED <abdulfareed355@gmail.com>
2025-11-05 18:29:29 +0530

    Force push local files

--- OOPS/program.py
+++ OOPS/program.py
@@ -70,4 +70,48 @@
 # basic()
 
 
+# num = "145"
+# sum = 0
+# for i in num:
+#     fact = 1
+#     for j in range(1,int(i)+1):
+#         fact *= j
+#     sum += fact
+# if num == str(sum):
+#     print('its a perfect number')
+# else:
+#     print("its not a perfect number")
+
+# inp = [12, "program", 6+78j, "kaiku"]
+# dt = {}
+
+# for i in inp:              
     
+#     if type(i) == str:     
+#         vowels = ""
+#         for j in i:         # go through each letter
+#             if j in "aeiouAEIOU":
+#                 vowels += j
+#         dt[i] = vowels      # store result
+
+# print(dt)
+
+
+
+# inp = [12, "program", 6+78j, "kaiku"]
+# dt = {}
+
+# for i in inp:              
+    
+#     if type(i) == str:     
+#         vowels = ""
+#         for j in i:         # go through each letter
+#             if j not in "aeiouAEIOU":
+#                 vowels += j
+#         dt[i] = vowels      # store result
+
+# print(dt)
+   
+
+inp = input("Enter the input: ").split()
+print(inp)
