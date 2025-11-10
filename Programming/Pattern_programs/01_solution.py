07424cd028877bf4c6a233c9c6188410bb95b532
QUANTUM'S FAREED <abdulfareed355@gmail.com>
2025-11-10 22:48:38 +0530

    Force push local files

--- Programming/Pattern_programs/01_solution.py
+++ Programming/Pattern_programs/01_solution.py
@@ -39,7 +39,7 @@
 #             print(" ",end = " ")
 #     print()
 
-print(chr(ord("A") +32))
+# print(chr(ord("A") +32))
 
 # n = 5
 # for i in range(1,n+1):
@@ -51,4 +51,29 @@ print(chr(ord("A") +32))
 #         else:
 #             print(" ",end=" ")
 #     print()
+
+# n = 5
+# char = "L"
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         if i==j or i < j:
+#             print(char,end=" ")
+#             char = chr(ord(char)+1)
+#         else:
+#             print(" ",end=" ")
+#     print()
             
+            
+n = 5
+
+for i in range(1,n+1):
+    m = i
+    for j in range(1,n+1):
+        if i % j == 0 or  i >= j:
+            print(m**2,end=" ")
+            m += 1
+        else:
+            print(" ",end=" ")
+    print()
+
+
