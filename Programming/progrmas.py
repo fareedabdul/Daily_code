4f94c256aa7d05ad3576c8dcaaaeef4e71f7385f
QUANTUM'S FAREED <abdulfareed355@gmail.com>
2025-11-12 19:27:49 +0530

    Force push local files

--- Programming/progrmas.py
+++ Programming/progrmas.py
@@ -23,4 +23,11 @@
 
 ##########################################################################################
 
-data = int(input("Enter a number to check Prime"))
+data = int(input("Enter a number to check Prime: "))
+i = 2
+while i < data:
+    if data % i == 0:
+        print("Not a prime re")
+        break
+    else:
+     print("Hmmm Its a prime Number")
--- Programming/tempCodeRunnerFile.py
+++ Programming/tempCodeRunnerFile.py
@@ -1,3 +1,8 @@
-
-# else:
-#     print("sorry mama")
+data = int(input("Enter a number to check Prime: "))
+i = 2
+while i < data:
+    if data % i == 0:
+        print("Not a prime re")
+        break
+    else:
+     print("Hmmm Its a prime Number")
