4cd6d96cae1b5a05c2373c5e322cea483228b3ee
QUANTUM'S FAREED <abdulfareed355@gmail.com>
2025-11-12 19:18:28 +0530

    Force push local files

---
+++ Programming/progrmas.py
@@ -0,0 +1,26 @@
+# data = input("Enter the String: ")
+# ch = input("Enter the character to check: ")
+# for i in range(len(data)):
+#     if data[0] == ch:
+#         print(f"Successfully its a Match {data[0]}!")
+#         break
+# else:
+#     print("Mama you Failed 😒 " ) 
+
+##################################################################################
+
+
+
+# data = input("Enter the string: ")
+# for i in data:
+#     if ('a' <= i <= 'z'):
+#         print("Succesfully")
+#         break
+# else:
+#     print("Not a lower case")
+
+
+
+##########################################################################################
+
+data = int(input("Enter a number to check Prime"))
