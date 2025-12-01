6762778ba557e30bcab8649a8a00bbc0b79b62a9
QUANTUM'S FAREED <abdulfareed355@gmail.com>
2025-12-01 22:29:55 +0530

    Force push local files

--- DSA/Extracting_the-data/program.py
+++ DSA/Extracting_the-data/program.py
@@ -1,7 +1,15 @@
-n = 7868
-while n > 0:
-    last_digit = n % 10
-    print(last_digit,end="")
-    n = n // 10
+# n = 7868
+# while n > 0:
+#     last_digit = n % 10
+#     print(last_digit,end="")
+#     n = n // 10
 
-    
+def fibnocci(n):
+    a = 0
+    b = 1
+    for i in range(n+1):
+        print(a,end="")
+        c = a+b
+        a = b
+        b = c
+fibnocci(5)  
